db9bab511fdce5cbb0011cb39ab2721e268d5262
Sahidulislam05 <sahidulislamcst@gmail.com>
2025-09-20 16:47:46 +0600

    first commit

---
+++ assets/script/main.js
@@ -0,0 +1,95 @@
+const loadData = () => {
+  const url = " https://taxi-kitchen-api.vercel.app/api/v1/categories";
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayCategory(data.categories));
+};
+
+const loadFoods = (id) => {
+  const url = ` https://taxi-kitchen-api.vercel.app/api/v1/categories/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayFoods(data.foods));
+};
+
+const loadFoodDetails = (id) => {
+  const url = `https://taxi-kitchen-api.vercel.app/api/v1/foods/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayModal(data.details));
+};
+const displayCategory = (categories) => {
+  //   console.log(categories);
+  const catContainer = document.getElementById("category-container");
+  catContainer.innerHTML = "";
+  for (let cat of categories) {
+    const categoryCard = document.createElement("div");
+    categoryCard.innerHTML = `
+          <button onclick="loadFoods(${cat.id})" class="btn justify-start btn-block shadow btn-category">
+            <img
+              src="${cat.categoryImg}"
+              alt=""
+              class="w-10"
+            /> ${cat.categoryName}
+          </button>
+    `;
+    catContainer.append(categoryCard);
+  }
+};
+
+const displayFoods = (foods) => {
+  const foodContainer = document.getElementById("food-container");
+  foodContainer.innerHTML = "";
+  foods.forEach((food) => {
+    const foodCard = document.createElement("div");
+    foodCard.innerHTML = `
+         <div onclick= "loadFoodDetails(${food.id})" class="p-5 bg-white flex gap-3 shadow rounded-xl">
+            <div class="img flex-1">
+              <img
+                src="${food.foodImg}"
+                alt=""
+                class="w-[160px] rounded-xl h-[160px] object-cover"
+              />
+            </div>
+            <div class="flex-2">
+              <h1 class="text-xl font-bold">
+                ${food.title}
+              </h1>
+
+              <div class="badge mt-2 badge-warning">${food.category} </div>
+
+              <div class="divider divider-end">
+                <h2 class="text-yellow-600 font-semibold">
+                  $ <span class="price">${food.price} </span> BDT
+                </h2>
+              </div>
+
+              <button class="btn btn-warning">
+                <i class="fa-solid fa-square-plus"></i>
+                Add This Item
+              </button>
+            </div>
+          </div>
+    `;
+    foodContainer.append(foodCard);
+  });
+};
+
+const displayModal = (food) => {
+  const detailsContainer = document.getElementById("details-container");
+  detailsContainer.innerHTML = `
+        <div>
+          <h2 class="text-3xl font-bold">${food.title} </h2>
+        </div>
+        <div>
+          <img class="mt-2" src="${food.foodImg}" alt="">
+        </div>
+        <div class= "badge badge-primary">
+        ${food.area}
+        </div>
+        <a href="${food.video}" target="_blank" class="btn btn-warning mt-3">Watch video</a>
+          `;
+  document.getElementById("my_modal_3").showModal();
+};
+
+loadData();
